280452449a2ef93cc47edab64b144951c294e026
sushmithr <sushmithramesh@gmail.com>
2019-12-14 17:10:54 -0500

    htmlFOR change

--- src/components/EditBar/AddProjectCard.tsx
+++ src/components/EditBar/AddProjectCard.tsx
@@ -94,7 +94,7 @@ function AddProjectCard(props:{index: number, project: Project, allProjects: Pro
     return (
         <Card>
             <CardContent>
-                <label for="title">
+                <label htmlFOR="title">
                     Project Title:
                 </label>
                 <br/>
@@ -136,7 +136,7 @@ function AddProjectCard(props:{index: number, project: Project, allProjects: Pro
                     </Select>
                 </FormControl>
                 <br/>
-                <label for="link">
+                <label htmlFOR="link">
                     Link:
                 </label>
                 <br/>
